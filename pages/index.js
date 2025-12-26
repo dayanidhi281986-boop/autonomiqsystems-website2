857fa69ac405bb5c27aacc17fd22fe22b019d301
dayanidhi281986-boop <dayanidhi281986@gmail.com>
2025-12-26 22:54:11 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      padding: "60px",
+      textAlign: "center"
+    }}>
+      <h1>AutonomIQ Systems Pvt. Ltd.</h1>
+      <h2>AI-Only IT Consulting, Operations & Development</h2>
+
+      <p>
+        Welcome to AutonomIQ Systems.
+      </p>
+
+      <p>
+        This company operates fully using autonomous artificial intelligence systems.
+      </p>
+    </div>
+  );
+}
